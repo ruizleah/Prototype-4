ca355d38e31670b30ddbfa495a3a46c36a60a64e
ruizleah <leruiz1@s.sfusd.edu>
2023-03-06 14:39:21 -0800

    4.2 done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,6 +18,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+        playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
     }
 }
