1101b6cc2d644ed74ca71bb1d35b60fd49d49126
ruizleah <leruiz1@s.sfusd.edu>
2023-03-21 13:58:21 -0700

    lesson 5 done

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -8,6 +8,7 @@ public class SpawnManager : MonoBehaviour
     private float spawnRange = 9;
     public int enemyCount;
     public int waveNumber = 1;
+    public GameObject powerupPrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +20,10 @@ public class SpawnManager : MonoBehaviour
     void Update()
     {
         enemyCount = FindObjectsOfType<Enemy>().Length;
-        if (enemyCount == 0) { waveNumber++; SpawnEnemyWave(waveNumber); }
+        if (enemyCount == 0)
+        { waveNumber++; SpawnEnemyWave(waveNumber);
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+        }
     }
 
     void SpawnEnemyWave(int enemiesToSpawn)
